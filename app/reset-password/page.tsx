f53a32123521fa0d274d8f1805c1c4e1d558bebe
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-09-28 16:47:05 +0100

    feat: accounting for error and success states

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -3,14 +3,17 @@
 import { useFormState } from "react-dom";
 import { resetPassword } from "./actions";
 
+const inputClasses =
+  "w-full px-4 py-2 rounded-full bg-slate-800 text-slate-50 border-2 border-slate-700 focus:border-indigo-500 focus:outline-none";
+const inputErrorClasses =
+  "w-full px-4 py-2 rounded-full bg-red-950 border-red-400 border-2 focus:outline-none";
+
 export default function ResetPassword() {
   const [state, formAction] = useFormState(resetPassword, {
     inputError: "",
     supabaseError: "",
     success: "",
   });
-
-  console.log(state);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-slate-950">
@@ -24,14 +27,23 @@ export default function ResetPassword() {
           id="password"
           type="password"
           placeholder="New Password"
-          className="w-full px-4 py-2 rounded-full bg-slate-800 text-slate-50 border-2 border-slate-700 focus:border-indigo-500 focus:outline-none"
+          className={state.inputError ? inputErrorClasses : inputClasses}
         />
+        {state.inputError && (
+          <p className="text-red-100 text-sm mt-2">{state.inputError}</p>
+        )}
         <button
           className="w-full px-4 py-2 rounded-full bg-slate-100 text-slate-900 mt-4"
           type="submit"
         >
           Reset Password
         </button>
+        {state.supabaseError && (
+          <p className="text-red-100 text-sm mt-2">{state.supabaseError}</p>
+        )}
+        {state.success && (
+          <p className="text-green-100 text-sm mt-2">{state.success}</p>
+        )}
       </form>
     </div>
   );
